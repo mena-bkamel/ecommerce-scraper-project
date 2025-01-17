b1562910eef9c02eaf044e9ff67cac2298afe97f
Mena Bebawy <menabebawy222@gmail.com>
2025-01-17 16:03:08 +0200

    create create_csv_file method

--- data_manipulation.py
+++ data_manipulation.py
@@ -2,12 +2,25 @@ import pandas as pd
 
 
 class DataManipulation:
-    def merge_files(self, files: tuple | list, new_file_name):
+
+    def merge_files(self, files: tuple | list):
         files_read = [pd.read_csv(file) for file in files]
         merged = pd.concat(files_read, ignore_index=True)
-        merged.to_csv(f"{new_file_name}.csv", index=False)
-        print("CSV files merged successfully!")
+        return merged
+
+    def create_csv_file(self, dataframe, file_name):
+
+        if isinstance(dataframe, pd.DataFrame):
+            if not file_name.endswith(".csv"):
+                file_name += ".csv"
+
+            dataframe.to_csv(file_name, index=False)
+        else:
+            print("Error: Input must be Pandas Dataframe.")
 
 
+if __name__ == '__main__':
+    dm = DataManipulation()
+    mer = dm.merge_files(["a.csv", "b.csv"])
 
-
+    dm.create_csv_file(mer, "amazon_file.csv")
